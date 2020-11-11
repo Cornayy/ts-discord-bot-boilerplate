ad97d49c9a409a025fc8d779d07e36c4269add79
Cornayy <38261196+Cornayy@users.noreply.github.com>
2020-11-11 14:36:03 +0100

    feat: initialization exception handling

--- src/Client.ts
+++ src/Client.ts
@@ -1,5 +1,6 @@
 import { Collection, Client as DiscordClient } from 'discord.js';
 import { Service } from 'typedi';
+import { Logger } from './utils/Logger';
 import { BotSettings, BotClient } from './types';
 import { Command } from './Command';
 import { ActionManager } from './managers/ActionManager';
@@ -11,16 +12,19 @@ export class Client extends DiscordClient implements BotClient {
 
     constructor(private actionManager: ActionManager) {
         super(configuration.clientOptions || {});
-
         this.settings = configuration;
         this.settings.token = process.env.BOT_TOKEN;
         this.initialize();
-        this.login(configuration.token);
     }
 
-    private initialize(): void {
-        this.actionManager.initializeCommands(this);
-        this.actionManager.initializeEvents(this);
+    private async initialize(): Promise<void> {
+        try {
+            await this.login(configuration.token);
+            this.actionManager.initializeCommands(this);
+            this.actionManager.initializeEvents(this);
+        } catch (e) {
+            Logger.error(`Could not initialize bot: ${e}`);
+        }
     }
 
     public get commands(): Collection<string, Command> {
